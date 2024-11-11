835df07597106850d9e92c365b39a2a2c5bb8e45
MaximSolodkovVMSIS <gt6600502@gmail.com>
2024-11-11 21:20:14 +0300

    change

--- Web/Program.cs
+++ Web/Program.cs
@@ -7,7 +7,7 @@ namespace Web
             var builder = WebApplication.CreateBuilder(args);
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            app.MapGet("/", () => "Hello!");
 
             app.Run();
         }
