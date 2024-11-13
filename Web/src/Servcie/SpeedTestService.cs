cd4345caf3402bb61188e02fdc4f4accbaf072bf
MaximSolodkovVMSIS <gt6600502@gmail.com>
2024-11-13 21:41:15 +0300

    add NetworkInformation lib

--- Web/src/Servcie/SpeedTestService.cs
+++ Web/src/Servcie/SpeedTestService.cs
@@ -1,10 +1,32 @@
-﻿namespace Web.src.Servcie
+﻿using Microsoft.Extensions.Primitives;
+using System.Net.NetworkInformation;
+using System.Text;
+using 
+
+namespace Web.src.Servcie
 {
     public class SpeedTestService : ISpeedTestService
     {
         public string GetSpeedTestResult()
         {
-            return "speed - 100 ms";
+            IPGlobalProperties computerProperties = IPGlobalProperties.GetIPGlobalProperties();
+            NetworkInterface[] nics = NetworkInterface.GetAllNetworkInterfaces();
+            if (nics == null || nics.Length < 1)
+            {
+                return "No network interfaces found";
+            }
+
+            StringBuilder result = new StringBuilder();
+
+            foreach (NetworkInterface adapter in nics)
+            {
+                IPInterfaceProperties properties = adapter.GetIPProperties();
+                result.Append("Name: " + adapter.Name + "\n");
+                result.Append("Physical Adress: " + adapter.GetPhysicalAddress().ToString() + "\n");
+                result.Append("Operational status: " + adapter.OperationalStatus + "\n");
+                
+            }
+            return "Number of interfaces: " + nics.Length + "\nHost Name " + computerProperties.HostName + "\n" + result;
         }
     }
 }
