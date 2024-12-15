47207a84880b5c9035e5c81ad9be43c6e2bc5f04
undertakerprog <gt6600502@gmail.com>
2024-12-15 23:31:29 +0300

    test

---
+++ Web/Src/Program.cs
@@ -0,0 +1,26 @@
+using Web.Src.Service;
+
+namespace Web
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddHttpClient();
+
+            builder.Services.AddScoped<IServerService, ServerService>();
+            builder.Services.AddScoped<IPingService, PingService>();
+            builder.Services.AddScoped<ILocationService, LocationService>();
+            builder.Services.AddScoped<ISpeedTestService, SpeedTestService>();
+
+            builder.Services.AddControllers();
+
+            var app = builder.Build();
+                
+            app.MapControllers();
+            app.Run();
+        }
+    }
+}
