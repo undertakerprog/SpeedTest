ec2bde45cfd3785e2031604b7367e1b7a7ea9083
undertakerprog <gt6600502@gmail.com>
2025-01-19 18:28:34 +0300

    add combobx with servers

--- Desktop/Views/MainPage.xaml.cs
+++ Desktop/Views/MainPage.xaml.cs
@@ -1,16 +1,27 @@
 ﻿using System.Net.Http;
+using System.Text.Json;
 using System.Windows;
 using Desktop.Services;
+using Desktop.Model;
+using System.Diagnostics;
 
 namespace Desktop.Views
 {
     public partial class MainPage
     {
+        private readonly HttpClient _httpClient = new HttpClient();
         private readonly SpeedTestService _speedTestService;
+        private Server _selectedServer;
+
         public MainPage()
         {
             InitializeComponent();
             _speedTestService = new SpeedTestService(new HttpClient { BaseAddress = new Uri("http://localhost:5252") });
+
+            SpeedTestRadioButton.Checked += RadioButton_Checked;
+            FastSpeedTestRadioButton.Checked += RadioButton_Checked;
+
+            _ = LoadServersAsync();
         }
 
         private async void StartButton_Click(object sender, RoutedEventArgs e)
@@ -35,7 +46,8 @@ namespace Desktop.Views
                 }
                 else
                 {
-                    MessageBox.Show("Select type for speedtest\n(Fast SpeedTest/SpeedTest)", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+                    MessageBox.Show("Select type for speedtest\n(Fast SpeedTest/SpeedTest)", "Information",
+                        MessageBoxButton.OK, MessageBoxImage.Information);
                 }
             }
             catch (Exception ex)
@@ -44,11 +56,55 @@ namespace Desktop.Views
             }
         }
 
-        private void ReloadButton_Click(object sender, RoutedEventArgs e)
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
         {
-            SpeedResultText.Visibility = Visibility.Collapsed;
+            ServersComboBox.Visibility = SpeedTestRadioButton.IsChecked == true
+                ? Visibility.Visible
+                : Visibility.Collapsed;
+        }
 
-            StartButton.Visibility = Visibility.Visible;
+        private async Task LoadServersAsync()
+        {
+            try
+            {
+                var response = await _httpClient.GetStringAsync("http://localhost:5252/api/location/servers-city-list");
+
+                var options = new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                };
+
+                var servers = JsonSerializer.Deserialize<List<Server>>(response, options);
+
+                if (servers == null || servers.Count == 0)
+                {
+                    MessageBox.Show("Список серверов пуст.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+
+                ServersComboBox.Items.Clear();
+                foreach (var server in servers)
+                {
+                    ServersComboBox.Items.Add($"{server.City}-{server.Provider}");
+                }
+
+                ServersComboBox.SelectionChanged += (sender, e) =>
+                {
+                    UpdateSelectedServer(servers);
+                };
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при загрузке серверов: {ex.Message}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void UpdateSelectedServer(List<Server> servers)
+        {
+            var selectedItem = ServersComboBox.SelectedItem as string;
+            if (string.IsNullOrEmpty(selectedItem)) return;
+            _selectedServer = servers.FirstOrDefault(s => $"{s.City}-{s.Provider}" == selectedItem);
+            Debug.WriteLine($"Selected server: {_selectedServer?.City}-{_selectedServer?.Provider}");
         }
     }
 }
